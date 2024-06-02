4db570860a7a24404cffc812ece97048bbb898bf
Magnus Urosev <95986408+murosev@users.noreply.github.com>
2024-06-02 10:57:17 -0500

    Changed settings for survey model

--- code/survey_form/survey_form_backend/settings.py
+++ code/survey_form/survey_form_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "django-insecure-cd-i=t%p6tg^%3mhc1*4un$%$#97yfihre7uw1j$68trflr3md"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  # Use this for prod DEBUG = config('DEBUG', default=False, cast=bool)
