8884f08f597b0a7e3d15766e01b85e58928617b0
IXZZZ9 <166889207+IXZZZ9@users.noreply.github.com>
2024-06-10 14:00:43 -0400

    Update admin.py
    Changing the registers for updating to new models.

--- code/survey_form/survey_app/admin.py
+++ code/survey_form/survey_app/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-from .models import Survey, Question, Response, Instructor, Student, Course, StudentCourse, ExamQuestion, InstructorWeightedQuestion, StudentExamQuestion
+from .models import CourseSurvey, CourseSurveyQuestion, Instructor, Student, Course, StudentCourse, CourseSurveyChoice, CourseSurveyResponse
 
-admin.site.register(Survey)
-admin.site.register(Question)
-admin.site.register(Response)
+admin.site.register(CourseSurvey)
+admin.site.register(CourseSurveyQuestion)
 admin.site.register(Instructor)
 admin.site.register(Student)
 admin.site.register(Course)
 admin.site.register(StudentCourse)
-admin.site.register(ExamQuestion)
-admin.site.register(InstructorWeightedQuestion)
-admin.site.register(StudentExamQuestion)
+admin.site.register(CourseSurveyChoice)
+admin.site.register(CourseSurveyResponse)
