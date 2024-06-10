c2d08641fdd9cfcace79da1f8fee3f04b26b7ae8
IXZZZ9 <166889207+IXZZZ9@users.noreply.github.com>
2024-06-10 01:59:42 -0400

    Adding the admin from models.

--- code/survey_form/survey_app/admin.py
+++ code/survey_form/survey_app/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Survey, Question, Response
+from .models import Survey, Question, Response, Instructor, Student, Course, StudentCourse, ExamQuestion, InstructorWeightedQuestion, StudentExamQuestion
 
 admin.site.register(Survey)
 admin.site.register(Question)
 admin.site.register(Response)
+admin.site.register(Instructor)
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(StudentCourse)
+admin.site.register(ExamQuestion)
+admin.site.register(InstructorWeightedQuestion)
+admin.site.register(StudentExamQuestion)
